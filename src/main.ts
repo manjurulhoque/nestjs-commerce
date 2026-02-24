d73aabb5c45516c4dd146cfb20505464d4547d03
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2026-02-25 00:39:50 +0600

    feat(logging): enhance bootstrap logging and configure NestJS logger for JSON output

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 
 const serverConfig = config.get('server');
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		logger: new ConsoleLogger({
+			json: true,
+		}),
+	});
 
 	const config = new DocumentBuilder()
 		.setTitle('NestJS E-commerce API')
@@ -19,5 +24,8 @@ async function bootstrap() {
 	});
 
 	await app.listen(serverConfig.port);
+
+	const logger = new Logger('bootstrap');
+	logger.log(`Server is running on ${await app.getUrl()}`);
 }
 bootstrap();
